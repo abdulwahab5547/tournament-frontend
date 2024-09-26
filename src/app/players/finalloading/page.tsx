2f7131986018cb91bab7a2fbbbe182934c787e46
Abdul Wahab Asif <136114691+abdulwahab5547@users.noreply.github.com>
2024-09-26 16:45:27 +0500

    Update page.tsx

--- src/app/players/finalloading/page.tsx
+++ src/app/players/finalloading/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Ball from '../../assets/ball.png'
+// import Ball from '../../assets/ball.png'
 import PhoneButtonLeft from '@/app/components/phonebtnleft'
 import PhoneButtonRight from '@/app/components/phonebtnright'
 
@@ -10,7 +10,7 @@ function FinalLoading(){
                 <div className="-rotate-6 text-lg font-extrabold uppercase flex flex-col items-center">
                     <p className="">Tournament</p>
                     <div className='flex gap-3'>
-                        <Image src={Ball} width={30} height={30} alt='' className='shadow-2xl'/>
+{/*                         <Image src={Ball} width={30} height={30} alt='' className='shadow-2xl'/> */}
                         <p className=''>App</p>
                     </div>
                 </div>
