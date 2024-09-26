4f4dfaa35264944fc99b994827ff2451b1e23c9d
Abdul Wahab Asif <136114691+abdulwahab5547@users.noreply.github.com>
2024-09-26 16:36:43 +0500

    Update page.tsx

--- src/app/players/teamregistered/page.tsx
+++ src/app/players/teamregistered/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from 'next/image';
-import Ball from '../../assets/ball.png';
+// import Ball from '../../assets/ball.png';
 import PhoneButtonLeft from '@/app/components/phonebtnleft';
 import PhoneButtonRight from '@/app/components/phonebtnright';
 import { useSearchParams } from 'next/navigation';
@@ -38,7 +38,7 @@ function TeamRegistered() {
                 <div className="-rotate-6 text-lg font-extrabold uppercase flex flex-col items-center">
                     <p>Tournament</p>
                     <div className='flex gap-3'>
-                        <Image src={Ball} width={30} height={30} alt='' className='shadow-2xl' />
+{/*                         <Image src={Ball} width={30} height={30} alt='' className='shadow-2xl' /> */}
                         <p>App</p>
                     </div>
                 </div>
